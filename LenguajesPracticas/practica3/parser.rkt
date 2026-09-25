562fbea5f949f1e163be503a3e288e700c7b79da
UrrIsaac <isaacurri@ciencias.unam.mx>
2026-09-24 23:04:22 -0600

    feat [parser.rkt] : termina preliminarmente parser.

--- LenguajesPracticas/practica3/parser.rkt
+++ LenguajesPracticas/practica3/parser.rkt
@@ -29,9 +29,7 @@ ALUMNOS :
           ( body WAE+?) ])
 
 
-; El trabajo del parser es pasar de s-expression a WAE+.
-; Como sabemos la gramática y las aridades de las operaciones tenemos que tenerlas :
-
+; Como conocemos la gramática y las aridades de las operaciones tenemos que tenerlas en algún sitio :
 ;; operaciones : symbol -> (procesimiento aridad)
 (define operaciones
   (list (list '+ + 'multi)
@@ -51,15 +49,10 @@ ALUMNOS :
    )
   )
 
-; Para buscar si un símbolo está en nuestra lista
-; symbol -> boolean
-(define (operaciones? simb)
-  (and (symbol? simb) (assq simb operaciones) ))
 
 
-(define (es-with? simb)
-  (memq simb '(with with*)))
-
+; Nuestra definición más importante
+;; parse : s-expression - > FWAE
 (define (parse sexp)
   (cond
     [(number? sexp) (num sexp)]
@@ -70,8 +63,8 @@ ALUMNOS :
         (id sexp))]
     [(list? sexp)
      (cond
-       [(eq? (first sexp) 'with) (parse-with sexp with )]
-       [(eq? (first sexp) 'with*) (parse-with sexp with* )]
+       [(eq? (first sexp) 'with) (parse-with sexp with #t)] ; true para ver que es with
+       [(eq? (first sexp) 'with*) (parse-with sexp with* #f)] ; false para ver que es with*
        [(operaciones? (first sexp)) (parse-op sexp)]
        [else (error 'parse "Syntax Error : expresion mal formada en parse") ]
        )]
@@ -79,24 +72,72 @@ ALUMNOS :
     )
   )
 
+; Para buscar si un símbolo está en nuestra lista
+;; symbol -> boolean
+(define (operaciones? simb)
+  (and (symbol? simb) (assq simb operaciones) ))
 
-; (define (parse-with ))
+; Para ver si es un with o un with*
+;; symbol -> boolean
+(define (es-with? simb)
+  (memq simb '(with with*)))
 
+;;
+(define (parse-op sexp)
+  (define op-sym (first sexp))
+  (define args (rest sexp))
+  (define n-args (length args))
+  (define definicion (assq op-sym operaciones))
+  (define proceso (second definicion))
+  (define aridad (third definicion))
+  (if (case aridad
+        [(uno) (= n-args 1) ]
+        [(dos) (= n-args 2) ]
+        [(multi) (>= n-args 2) ]
+        [else #f ]
+        )
+      (op proceso (map parse args))
+      (error 'parse "Syntax Error : expresion mal formada en parse")
+      )
+  )
 
-; (define (parse-op ))
-
-
-(define (parse-with sexp constructor check-dup?)
-  (if (and (= (length sexp) 3) (list? (second sexp)) (>= (length (second sexp)) 1))
+;;
+(define (parse-with sexp constructor tipo)
+  (if (and (= (length sexp) 3) (list? (second sexp)) (>= (length (second sexp)) 1) )
       (let* ([bindings-sexp (second sexp)]
-             [body-sexp     (third sexp)]
-             [bindings      (map parse-binding bindings-sexp)]
-             [ids           (map binding-id bindings)])
-        (if (and check-dup? (has-duplicates? ids))
-            (error 'parse "Syntax Error: expresion mal formada en parse")
-            (constructor bindings (parse body-sexp))))
-      (error 'parse "Syntax Error: expresion mal formada en parse")))
+             [body-sexp (third sexp)]
+             [bindings (map parse-binding bindings-sexp)]
+             [ids (map binding-id bindings)]) ; accessor del campo id
+        (if (and tipo (tiene-duplicados? ids))
+            (error 'parse "Syntax Error : expresion mal formada en parse")
+            (constructor bindings (parse body-sexp))
+            )
+        )
+      (error 'parse "Syntax Error : expresion mal formada en parse")
+      )
+  )
 
+(define (parse-binding b)
+  (if(and (list? b) (= (length b) 2) (symbol? (first b)) (not (es-with? (first b))) )
+     (binding (first b) (parse (second b)))
+     (error 'parse "Syntax Error : expresion mal formada en parse")
+     )
+  )
+
+(define (tiene-duplicados? lista)
+  (not (= (length lista) (length (remove-duplicates lista))))
+  )
+
+
+
+; ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+; un parse-binding de b, debe ver si es una lista, su longitud es dos, el primero es un símbolo y no es reservados (with ((with x)) (+ x 5)) no
+; si cumple entonces hacemos binding del primero con el parseo del segundo
+; si no cumple es un error
+
+
+
+; el has-duplicados? es de una lista, es el not de si es igual la longitud de la lista con el remove-duplicates de la lista
 
 
 ; a ver, tengo mi lista de operaciones, tengo mi gramática y mi binding
@@ -112,6 +153,9 @@ ALUMNOS :
 
 ;                      (define b (binding 'x (num 5)))
 ;                      (binding-id b)     ; => 'x
+
+
+
 ;  si es operación entonces parseo la operación, debo checar aridad (uno, dos multi, si no #f) si sí un (op proc (map parse arg-sexps)), si no error.
 
 ; un parse-binding de b, debe ver si es una lista, su longitud es dos, el primero es un símbolo y no es reservados (with ((with x)) (+ x 5)) no
